2b8c3c91c50891f25bfadade24769e20bc6b0af5
ragg96 <grodrigo05@gmail.com>
2023-10-11 16:04:48 -0600

    added test code to led4.py

--- Toy_projects/led4.py
+++ Toy_projects/led4.py
@@ -10,5 +10,8 @@ led = {
 
 while True:
     for i in led.keys():
+        for j in led.keys():
+            if i != j:
+                led[j].off()
         led[i].blink(on_time=1, off_time=1)
         sleep(2)
