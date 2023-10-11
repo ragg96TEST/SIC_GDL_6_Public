013ef8a2b616235142b7c462e4bc1c0da48f3618
ragg96 <grodrigo05@gmail.com>
2023-10-11 15:45:52 -0600

    added test code to led4.py

---
+++ Toy_projects/led4.py
@@ -0,0 +1,14 @@
+from gpiozero import LED
+from time import sleep
+
+led = {
+    "red" : LED(17),
+    "yellow" : LED(6),
+    "green" : LED(13)
+
+}
+
+while True:
+    for i in led.keys():
+        led[i].blink(on_time=1, off_time=1)
+        sleep(2)
